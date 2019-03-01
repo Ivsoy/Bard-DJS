343a8a99012445852f29003fd931554dda519605
Ivsoy <ivsoy@icloud.com>
2019-03-01 01:02:47 +0100

    Basic Setup

--- App.js
+++ App.js
@@ -1,2 +1,20 @@
 // Require Packages
-// Discord JS
+const discord = require('discord.js');
+const client = new discord.Client();
+const { token } = require('./config.json');
+
+// Client Login
+client.login(token);
+
+// Client Startup Process
+client.on('ready', () => {
+    // Set Client Status
+    client.user.setActivity('My Developer', { type: 'LISTENING' });
+    console.log('Connecting as ' + client.user.tag + ' in ' + client.guilds.size + ' server(s)');
+});
+
+// Global Commands
+client.on('message', msg => {
+    // Ignore Commands From Bots
+    if (msg.author.bot) return;
+});
